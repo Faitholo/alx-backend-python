fd9e4bd9f91b3f7ce42c33c2805ed923e703c688
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-14 21:29:46 +0100

    task 3

---
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+"""A type-annotated function that takes a float and returns a string representation"""
+import math
+
+
+def to_str(n: float) -> str:
+    """this function  returns a string representation"""
+    return str(n)
