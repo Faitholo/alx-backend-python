daa4163167199a3aa043f0f8a8f160ca902ed3ef
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-14 19:34:10 +0100

    task 0

---
+++ 0x00-python_variable_annotations/0-add.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""A type-annotated function that returns the sum of two floats """
+
+def add(a: float, b: float) -> float:
+    """adds two floats and return sum as float"""
+    return a + b
