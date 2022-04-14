3b59bee860e1deb3cef6a57181dfd37a5d114bfa
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-14 20:27:22 +0100

    task 2

---
+++ 0x00-python_variable_annotations/2-floor.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"""A type-annotated function that returns a concatenated string """
+
+
+import math
+
+def floor(n: float) -> float:
+    """this function  returns a concatenated string"""
+    return math.floor(n)
