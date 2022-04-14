fee229eb439773435c784e584283da111b77aacf
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-14 22:40:13 +0100

    task 6

---
+++ 0x00-python_variable_annotations/6-sum_mixed_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""annotated function, takes floats as argument, returns sum as float"""
+from typing import List, Union
+
+
+def sum_mixed_list(mxd_lst: List[Union[int, float]]) -> float:
+    return sum(mxd_lst)
