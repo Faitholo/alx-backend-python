e89fb71bc6947a541a5851cfbebf189d64fd32e5
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-14 22:07:19 +0100

    task 5

---
+++ 0x00-python_variable_annotations/5-sum_list.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""annotated function, takes floats as argument, returns their sum as a float"""
+from typing import List
+
+
+def sum_list(input_list: List[float] = []) -> float:
+    return sum(input_list)
