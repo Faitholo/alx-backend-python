6bd9763be0b0b4a07ddfabdb9bf14be8bca6749a
Faith Ikhidero <ikhidero.faithy@gmail.com>
2022-04-14 19:51:53 +0100

    task 1

---
+++ 0x00-python_variable_annotations/1-concat.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+"""A type-annotated function that returns a concatenated string """
+
+
+def concat(str1: str, str2: str) -> str:
+    """this function  returns a concatenated string"""
+    return str1 + str2
